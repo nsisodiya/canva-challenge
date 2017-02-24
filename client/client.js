bbeb97e1d1371e2e1e025649ef97f4fe04701b20
Narendra Sisodiya <narendra@narendrasisodiya.com>
2017-02-24 17:39:59 +0530

    Removed Hardcoded value - Now all 7 tests are passing.

--- client/client.js
+++ client/client.js
@@ -2,5 +2,10 @@
 // Feel free to add other JS files in this directory as you see fit.
 
 define("client", function (Tournament) {
-  new Tournament(3, 9);
+  document.querySelector("#start").addEventListener("click", function () {
+    //TODO - Add Validation
+    const teamsPerMatch = parseInt(document.querySelector("#teamsPerMatch").value);
+    const numberOfTeams = parseInt(document.querySelector("#numberOfTeams").value);
+    new Tournament(teamsPerMatch, numberOfTeams);
+  }, false);
 });
